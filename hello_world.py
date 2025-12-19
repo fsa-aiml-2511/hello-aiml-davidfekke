dacbb50fae966d03ad498f76b9dfe5af9f7d1f53
davidfekke <david@fekke.com>
2025-12-18 20:47:24 -0500

    This is is my first commit

--- hello_world.py
+++ hello_world.py
@@ -1,8 +1,8 @@
 # Hello AI/ML Bootcamp!
 # Edit the variables below to introduce yourself
 
-name = "Your Name Here"
-fun_fact = "One fun fact about me"
+name = "David Fekke"
+fun_fact = "I love git!"
 
 # Don't change anything below this line
 print(f"Hello! I'm {name}.")
